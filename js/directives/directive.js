d8449657adc5f04775c55460249f17d841ac95ad
Kevin Mendoza Tudares <kevinamendoza@users.noreply.github.com>
2019-12-20 13:37:47 -0600

    angular

---
+++ js/directives/directive.js
@@ -0,0 +1,9 @@
+linkToController.directive('icon-link', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      info: '='
+    },
+    templateUrl: 'https://kevinamendoza.github.io/js/directives/iconLink.html'
+  }
+});
