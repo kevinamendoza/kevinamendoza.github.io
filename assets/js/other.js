314ed6f6c1379387e831933e0119479411207ebb
Kevin Mendoza Tudares <kevinmendozatudares2022@u.northwestern.edu>
2020-11-11 00:12:37 -0600

    apply redesign, remove unneccesary assets

--- assets/js/other.js
+++
@@ -1,7 +0,0 @@
-function showPhone() {
-  var phoneNumber = document.getElementById("myPhone")
-  if (phoneNumber.style.display === "none") {
-    phoneNumber.style.display = "block";
-    document.getElementById("showPhoneButton").style.display = "none";
-  }
-}
